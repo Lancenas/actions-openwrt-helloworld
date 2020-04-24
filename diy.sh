59d8e8983184567b551f4f028cddaa7cb170b2d6
Lancenas <32763979+Lancenas@users.noreply.github.com>
2020-04-24 14:34:25 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,5 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+rm -rf package/lienol
+git clone https://github.com/Lancenas/lienol-openwrt-package/lienol package/lienol
