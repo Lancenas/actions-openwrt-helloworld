841f217bf8902f9a966f4bd470321d32ca201730
Lancenas <32763979+Lancenas@users.noreply.github.com>
2020-04-26 12:11:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,4 @@
 #============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
