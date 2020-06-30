59eb240761a33b239882e94d0c34170d7bf9d10d
wg801020 <58475078+wg801020@users.noreply.github.com>
2020-06-30 17:53:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,4 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
